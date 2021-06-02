09afc1287a83abcfedc53028e8bb6ea8673d0f1c
unknown <zhaopuwa@usc.edu>
2021-06-01 21:06:26 -0700

    Two direction notes, bug: not changing color.

--- NARG2D/Assets/Scripts/Note.cs
+++ NARG2D/Assets/Scripts/Note.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Note : MonoBehaviour
 {
-    public float speed = 1f;
+    public float speed;
     private Renderer renderer;
     private GameObject circle;
     private Renderer circleRenderer;
@@ -22,44 +22,30 @@ public class Note : MonoBehaviour
         transform.Translate(Vector3.right * speed * Time.deltaTime);
     }
 
+    /*
     void OnCollisionStay2D(Collision2D collision)
     {
-        
         if (Input.anyKey)
         {
+
             if (collision.gameObject.tag == "Circle")
             {
-                circleRenderer.material.SetColor("_Color", Color.yellow);
-                //StartCoroutine(Waitforit());
-                Destroy(this.gameObject);
-            }
-            if (collision.gameObject.tag == "Capsule")
-            {
                 circleRenderer.material.SetColor("_Color", Color.green);
-                //StartCoroutine(Waitforit());
-                Destroy(this.gameObject);
-
+                StartCoroutine(Waitforit());
+                //Destroy(this.gameObject);
             }
 
         }
     }
+    */
 
-    void OnCollisionExit2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Circle")
-        {
-            renderer.material.SetColor("_Color", Color.red);
-        }
-        if (collision.gameObject.tag == "Ground")
+
+        if (collision.gameObject.tag == "Capsule")
         {
             Destroy(this.gameObject);
         }
     }
 
-    IEnumerator Waitforit()
-    {
-        yield return new WaitForSeconds(0.5f);
-        circleRenderer.material.SetColor("_Color", Color.white);
-        Destroy(this.gameObject);
-    }
 }
--- NARG2D/Assets/Scripts/NoteSystem.cs
+++ NARG2D/Assets/Scripts/NoteSystem.cs
@@ -5,45 +5,65 @@ using UnityEngine;
 public class NoteSystem : MonoBehaviour
 {
     public int bpm;
+    public float speed;
     private float beatRate;
-    public GameObject note;
-    public GameObject circle;
+    public Note note;
     private Renderer circleRenderer;
     public GameObject capsule;
-    private Vector2 pos;
-    private float nextTick = 0f;
-    private bool color;
+    private Vector2 leftPos;
+    private Vector2 rightPos;
+    private bool collision = false;
     // Start is called before the first frame update
     void Start()
     {
         beatRate = 60f / bpm;
-        pos = new Vector2(-8f, -3f);
-        circleRenderer = circle.GetComponent<Renderer>();
+        leftPos = new Vector2(-9f, -3f);
+        rightPos = new Vector2(9f, -3f);
+        circleRenderer = GetComponent<Renderer>();
         InvokeRepeating("SpawnNote", 0f, beatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.anyKeyDown)
+        {
+            if (collision)
+                ChangeColor();
+
+            GameObject[] notes = GameObject.FindGameObjectsWithTag("Note");
+            //Debug.Log(notes.Length + " notes on screen\n");
+            Destroy(notes[0].gameObject);
+            Destroy(notes[1].gameObject);
+        }
     }
 
     private void SpawnNote()
     {
-        Instantiate(note, pos, Quaternion.identity);
+        Note leftNote = Instantiate(note, leftPos, Quaternion.identity);
+        leftNote.speed = speed;
+        Note rightNote = Instantiate(note, rightPos, Quaternion.identity);
+        rightNote.speed = -speed;
         //ToggleColor();
     }
 
-    private void ToggleColor()
+    private void OnCollisionEnter2D()
     {
-        if (color) {
-            circleRenderer.material.SetColor("_Color", Color.blue);
-            color = false;
-        }
-        else
-        {
-            circleRenderer.material.SetColor("_Color", Color.white);
-            color = true;
-        }
+        Debug.Log("collision detected\n");
+        collision = true;
+    }
+    private void OnCollisionExit2D()
+    {
+        Debug.Log("collision exit\n");
+        collision = false;
     }
 
+    private IEnumerator ChangeColor()
+    {
+        circleRenderer.material.SetColor("_Color", Color.green);
+        yield return new WaitForSeconds(0.5f);
+        Debug.Log("Setting color to white\n");
+        circleRenderer.material.SetColor("_Color", Color.white);
+        //Destroy(this.gameObject);
+    }
 }
