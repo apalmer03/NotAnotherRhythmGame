cdaa36700cbd21fabd7e69f53e7f21d7ee25e929
lungyoungyu <lungyouy@usc.edu>
2021-06-01 21:05:36 -0700

    Added attack, uppercut, block. No double jumping.

--- NARG2D/Assets/Scripts/MainCharacterController.cs
+++ NARG2D/Assets/Scripts/MainCharacterController.cs
@@ -10,46 +10,93 @@ public class MainCharacterController : MonoBehaviour
     private Rigidbody2D rigidbody;
     private bool isGrounded = true;
     private float jumpVelocity = 25f;
+    private float moveVelocity = 3f;
+    private Color heroColor = new Color32(4, 160, 248, 255);
+    private Vector3 heroStartPosition;
 
     // Start is called before the first frame update
     void Start()
     {
+    	RenderSettings.ambientLight = Color.white;
+
         rigidbody = GetComponent<Rigidbody2D>();
         enemyRenderer = enemy.GetComponent<Renderer>();
         selfRenderer = GetComponent<Renderer>();
+        heroStartPosition = new Vector3((float)-4.000059, 0, (float)1.5);
+        rigidbody.transform.position = heroStartPosition;
+        selfRenderer.transform.position = heroStartPosition;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {	
+    	// Jump (No double jumping)
         if (Input.GetKeyDown("space") && isGrounded)
         {
-            isGrounded = false;
             rigidbody.velocity = Vector2.up * jumpVelocity;
         }
 
-        if (Input.GetKey(KeyCode.F))
+        // Attack (Dash Right)
+        if (Input.GetKey(KeyCode.J))
         {
             StartCoroutine(Attack());
+        }
+
+        if (Input.GetKey(KeyCode.K))
+        {
+        	StartCoroutine(Uppercut());
+        }
+
+        // Block
+        if (Input.GetKey(KeyCode.S))
+        {
+        	StartCoroutine(Block());
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            isGrounded = true;
-        }
-    }
+	{
+	    if (collision.gameObject.CompareTag("Ground"))
+	    {
+	        isGrounded = true;
+	    }
+	}
+
+    void OnCollisionExit2D(Collision2D collision)
+	{
+	    if (collision.gameObject.CompareTag("Ground"))
+	    {
+	        isGrounded = false;
+	    }
+	}
 
     IEnumerator Attack()
     {
+    	rigidbody.transform.position = new Vector3(3, 0, 0);
         enemyRenderer.material.SetColor("_Color", Color.black);
         selfRenderer.material.SetColor("_Color", Color.white);
         yield return new WaitForSeconds(0.2f);
         enemyRenderer.material.SetColor("_Color", Color.red);
-        Color blueColor = new Color32(4, 160, 248, 255);
-        selfRenderer.material.SetColor("_Color", blueColor);
+        selfRenderer.material.SetColor("_Color", heroColor);
+        rigidbody.transform.position = heroStartPosition;
+    }
+
+    IEnumerator Uppercut()
+    {
+    	rigidbody.transform.position = new Vector3(3, 3, 0);
+        enemyRenderer.material.SetColor("_Color", Color.black);
+        selfRenderer.material.SetColor("_Color", Color.white);
+        yield return new WaitForSeconds(0.2f);
+        enemyRenderer.material.SetColor("_Color", Color.red);
+        selfRenderer.material.SetColor("_Color", heroColor);
+        rigidbody.transform.position = heroStartPosition;
+    }
+
+    IEnumerator Block()
+    {
+    	selfRenderer.material.SetColor("_Color", Color.green);
+    	yield return new WaitForSeconds(0.5f);
+        selfRenderer.material.SetColor("_Color", heroColor);
     }
 
 }
