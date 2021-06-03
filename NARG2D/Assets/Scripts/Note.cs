06164f86fda3bf13b806a4ca2268520f821fba0e
Vivian Zhu <tongyu@usc.edu>
2021-06-03 12:12:29 -0700

    update song position beat hits

--- NARG2D/Assets/Scripts/Note.cs
+++ NARG2D/Assets/Scripts/Note.cs
@@ -4,48 +4,60 @@ using UnityEngine;
 
 public class Note : MonoBehaviour
 {
-    public float speed;
-    private Renderer renderer;
-    private GameObject circle;
-    private Renderer circleRenderer;
-    // Start is called before the first frame update
-    void Start()
+  public float speed;
+  private Renderer renderer;
+  private GameObject circle;
+  private Renderer circleRenderer;
+  public float livingTime = 0f;
+  public float lifeSpan;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    renderer = GetComponent<Renderer>();
+    circle = GameObject.FindGameObjectWithTag("Circle");
+    circleRenderer = circle.GetComponent<Renderer>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    livingTime += Time.deltaTime;
+    if (livingTime >= lifeSpan)
     {
-        renderer = GetComponent<Renderer>();
-        circle = GameObject.FindGameObjectWithTag("Circle");
-        circleRenderer = circle.GetComponent<Renderer>();
+      // Kills the game object
+      Destroy(gameObject);
+
+      // Removes this script instance from the game object
+      Destroy(this);
     }
+    transform.Translate(Vector2.right * speed * Time.deltaTime);
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
-    }
+  /*
+  void OnCollisionStay2D(Collision2D collision)
+  {
+      if (Input.anyKey)
+      {
 
-    /*
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        if (Input.anyKey)
-        {
+          if (collision.gameObject.tag == "Circle")
+          {
+              circleRenderer.material.SetColor("_Color", Color.green);
+              StartCoroutine(Waitforit());
+              //Destroy(this.gameObject);
+          }
 
-            if (collision.gameObject.tag == "Circle")
-            {
-                circleRenderer.material.SetColor("_Color", Color.green);
-                StartCoroutine(Waitforit());
-                //Destroy(this.gameObject);
-            }
+      }
+  }
+  */
 
-        }
-    }
-    */
+  // void OnCollisionEnter2D(Collision2D collision)
+  // {
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-
-        if (collision.gameObject.tag == "Capsule")
-        {
-            Destroy(this.gameObject);
-        }
-    }
+  //     if (collision.gameObject.tag == "Capsule")
+  //     {
+  //         Destroy(this.gameObject);
+  //     }
+  // }
 
 }
--- NARG2D/Assets/Scripts/NoteSystem.cs
+++ NARG2D/Assets/Scripts/NoteSystem.cs
@@ -4,66 +4,133 @@ using UnityEngine;
 
 public class NoteSystem : MonoBehaviour
 {
-    public int bpm;
-    public float speed;
-    private float beatRate;
-    public Note note;
-    private Renderer circleRenderer;
-    public GameObject capsule;
-    private Vector2 leftPos;
-    private Vector2 rightPos;
-    private bool collision = false;
-    // Start is called before the first frame update
-    void Start()
+  public int bpm;
+  public float speed;
+  private float beatRate;
+  public Note note;
+  private Renderer circleRenderer;
+  public GameObject capsule;
+  private Vector2 leftPos;
+  private Vector2 rightPos;
+  private bool collision = false;
+
+  // The number of seconds for each song beat
+  public float secPerBeat;
+
+  // Current song position, in seconds
+  public float songPosition;
+
+  // Current song position, in beats
+  public float songPositionInBeats;
+
+  // How many seconds have passed since the song started
+  public float dspSongTime;
+
+  // an AudioSource attached to this GameObject that will play the music.
+  public AudioSource musicSource;
+
+  // The offset to the first beat of the song in seconds
+  public float firstBeatOffset;
+
+  // keep all the position-in-beats of notes in the song
+  float[] notes;
+
+  // the index of the next note to be spawned
+  int nextIndex = 0;
+
+  int beatsShownInAdvance = 0;
+
+  int currentBeat = 0;
+
+  float marginOfError = 0.3f;
+
+  private IEnumerator coroutine;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    secPerBeat = 60f / bpm;
+    leftPos = new Vector2(-9f, -3f);
+    rightPos = new Vector2(9f, -3f);
+    GameObject circle = GameObject.FindGameObjectWithTag("Circle");
+    circleRenderer = circle.GetComponent<Renderer>();
+    // InvokeRepeating("SpawnNote", 0f, secPerBeat);
+
+    // Load the AudioSource attached to the Conductor GameObject
+    musicSource = GetComponent<AudioSource>();
+
+    // Record the time when the music starts
+    dspSongTime = (float)AudioSettings.dspTime;
+
+    // Initialize notes array
+    notes = new float[200];
+    for (var i = 0; i < 200; i++)
     {
-        beatRate = 60f / bpm;
-        leftPos = new Vector2(-9f, -3f);
-        rightPos = new Vector2(9f, -3f);
-        circleRenderer = GetComponent<Renderer>();
-        InvokeRepeating("SpawnNote", 0f, beatRate);
+      notes[i] = i;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.anyKeyDown)
-        {
-            if (collision)
-                ChangeColor();
+    // Start the music
+    musicSource.Play();
+  }
 
-            GameObject[] notes = GameObject.FindGameObjectsWithTag("Note");
-            //Debug.Log(notes.Length + " notes on screen\n");
-            Destroy(notes[0].gameObject);
-            Destroy(notes[1].gameObject);
-        }
-    }
-
-    private void SpawnNote()
+  // Update is called once per frame
+  void Update()
+  {
+    // determine how many seconds since the song started
+    songPosition = (float)(AudioSettings.dspTime - dspSongTime - firstBeatOffset);
+    // determine how many beats since the song started
+    songPositionInBeats = songPosition / secPerBeat;
+    // update current beat if passed
+    if (songPositionInBeats > notes[currentBeat])
     {
-        Note leftNote = Instantiate(note, leftPos, Quaternion.identity);
-        leftNote.speed = speed;
-        Note rightNote = Instantiate(note, rightPos, Quaternion.identity);
-        rightNote.speed = -speed;
-        //ToggleColor();
+      currentBeat++;
     }
 
-    private void OnCollisionEnter2D()
+    if (nextIndex < notes.Length && notes[nextIndex] < songPositionInBeats + beatsShownInAdvance)
     {
-        Debug.Log("collision detected\n");
-        collision = true;
-    }
-    private void OnCollisionExit2D()
-    {
-        Debug.Log("collision exit\n");
-        collision = false;
+      SpawnNote();
+      //initialize the fields of the music note
+      nextIndex++;
     }
 
-    private IEnumerator ChangeColor()
+    if (Input.anyKeyDown)
     {
-        circleRenderer.material.SetColor("_Color", Color.green);
-        yield return new WaitForSeconds(0.5f);
-        Debug.Log("Setting color to white\n");
-        circleRenderer.material.SetColor("_Color", Color.white);
-        //Destroy(this.gameObject);
+      Debug.Log(currentBeat + "\n");
+      Debug.Log(songPositionInBeats + "\n");
+      //   Debug.Log(Mathf.Abs(songPositionInBeats - notes[currentBeat]));
+      //   Debug.Log(Mathf.Abs(songPositionInBeats - notes[currentBeat]) <= marginOfError);
+      float err = Mathf.Abs(songPositionInBeats - notes[currentBeat]);
+      // check if hit on beat
+      if (err <= marginOfError)
+      {
+        coroutine = ChangeColor(0.3f);
+        StartCoroutine(coroutine);
+      }
+      // check if not on beat
+      else
+      {
+        GameObject[] notes = GameObject.FindGameObjectsWithTag("Note");
+        //Debug.Log(notes.Length + " notes on screen\n");
+        Destroy(notes[0].gameObject);
+        Destroy(notes[1].gameObject);
+        currentBeat++;
+      }
     }
+  }
+
+  private void SpawnNote()
+  {
+    Note leftNote = Instantiate(note, leftPos, Quaternion.identity);
+    leftNote.speed = speed;
+    leftNote.lifeSpan = Mathf.Abs(leftPos.x / speed);
+    Note rightNote = Instantiate(note, rightPos, Quaternion.identity);
+    rightNote.speed = -speed;
+    rightNote.lifeSpan = Mathf.Abs(rightPos.x / speed);
+  }
+  private IEnumerator ChangeColor(float waitTime)
+  {
+    circleRenderer.material.SetColor("_Color", Color.green);
+    yield return new WaitForSeconds(waitTime);
+    circleRenderer.material.SetColor("_Color", Color.white);
+  }
 }
