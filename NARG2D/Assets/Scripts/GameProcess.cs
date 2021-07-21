0a860df80322e6c3e0513d9d5b1727fc2da421e6
Tongyu Zhu <tongyu@usc.edu>
2021-07-20 20:31:45 -0700

    add extra buttons

--- NARG2D/Assets/Scripts/GameProcess.cs
+++ NARG2D/Assets/Scripts/GameProcess.cs
@@ -150,12 +150,14 @@ public class GameProcess : MonoBehaviour
 
     public void Retry()
     {
+        ResumeGame();
         Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void Back()
     {
+        ResumeGame();
         Time.timeScale = 1;
         SceneManager.LoadScene("MainMenu");
     }
